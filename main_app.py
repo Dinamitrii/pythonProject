0788643a89debcd6dd495287434b29c02e66626b
dinamitrii <k_karaivanov@abv.bg>
2024-10-16 22:11:36 +0300

    added ignore

--- main_app.py
+++ main_app.py
@@ -12,14 +12,16 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus"
            "car", "cat", "chair", "cow", "dining table", "dog", "horse",
            "motorbike", "person", "potted plant", "sheep", "sofa", "train", "tv-monitor"]
 
+id_xternal = int()
+
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 net = cv2.dnn.readNetFromCaffe("ssd_files/MobileNetSSD_deploy.prototxt", "ssd_files/MobileNetSSD_deploy.caffemodel")
 
 if use_gpu:
     print("[INFO] setting preferable backend to OpenCV and preferable target to OpenCL...")
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL_FP16)
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_VKCOM)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_VULKAN)
 
 print("[INFO] accessing video stream...")
 
@@ -41,15 +43,15 @@ while ret:
         for i in np.arange(0, detections.shape[2]):
             confidence = detections[0, 0, i, 2]
             if confidence > confidence_level:
-                idx = int(detections[0, 0, i, 1])
+                id_xternal = int(detections[0, 0, i, 1])
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
 
-            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
+            label = "{}: {:.2f}%".format(CLASSES[id_xternal], confidence * 100)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[id_xternal], 2)
 
             y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[id_xternal], 2)
 
         cv2.imshow("Live detection", frame)
 
